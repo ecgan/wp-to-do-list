b76ae41e9802695c1b30dd3451d0d0feee2f0f41
Gan Eng Chin <ecgan@users.noreply.github.com>
2024-05-14 00:35:06 +0800

    Add admin menu main page.

--- wp-to-do-list.php
+++ wp-to-do-list.php
@@ -40,3 +40,36 @@ function register_types() {
 }
 
 add_action('init', 'register_types');
+
+function wporg_options_main_html() {
+    ?>
+    <div class="wrap">
+      <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+      <form action="options.php" method="post">
+        <?php
+        // output security fields for the registered setting "wporg_options"
+        settings_fields( 'wporg_options' );
+        // output setting sections and their fields
+        // (sections are registered for "wporg", each field is registered to a specific section)
+        do_settings_sections( 'wporg' );
+        // output save settings button
+        submit_button( __( 'Save Settings', 'textdomain' ) );
+        ?>
+      </form>
+    </div>
+    <?php
+}
+
+function add_admin_menu_main_page() {
+    add_menu_page(
+        __( 'WP To-do List', 'wp-to-do-list' ),
+        __( 'WP To-do List 123', 'wp-to-do-list' ),
+        'manage_options',
+        'wp-to-do-list_main',
+        'wporg_options_main_html',
+        '',
+        20
+    );
+}
+
+add_action( 'admin_menu', 'add_admin_menu_main_page' );
